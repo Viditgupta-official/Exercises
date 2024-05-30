423d02773fc5739cab6916c35c0c02625868806d
Viditgupta-official <viditgupta.official@gmail.com>
2024-05-30 22:29:53 +0530

    Dunno what happened

--- Exponential_Function.c
+++ Exponential_Function.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
 
-int power(int x,int y);
+int exponential(int x,int y);
 
 int main()
 
 {
     int a, b, expo;
 
-    while(1)
     printf("Enter a base number : ");
     scanf("%d", &a);
 
     printf("Enter it's power : ");
     scanf("%d", &b);
 
-    expo = power(a, b);
+    expo = exponential(a, b);
 
     printf("The value of %d raised to the power of %d is = %d", a, b, expo);
 }
 
-int power(int x, int y)
+int exponential(int x, int y)
 
 {
     for (int i = y; i > 1; i--)
