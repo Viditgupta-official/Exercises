922c2b579193bb6f626104ed7281f56580a92c3b
Viditgupta-official <viditgupta.official@gmail.com>
2024-05-30 16:43:41 +0530

    New Program for calculating exponential of a number input by the user

---
+++ Exponential_Function.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int power(int x,int y);
+int main()
+
+{
+    int a, b, exponential;
+
+    printf("Enter a base number : ");
+    scanf("%d", &a);
+
+    printf("Enter it's power : ");
+    scanf("%d", &b);
+
+    exponential = power(a, b);
+
+    printf("The value of %d raised to the power of %d is = %d", a, b, exponential);
+}
+
+int power(int x, int y)
+
+{
+    for (int i = y; i > 1; i--)
+    {
+        x = x*x;
+    }
+
+    return(x);
+}
